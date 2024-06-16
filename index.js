f80540561290ece23924c889477bed42667ef9c5
BoAmarant <bobamarant8@gmail.com>
2024-06-16 17:00:16 -0400

    Added code to index.js, still need to test

--- index.js
+++ index.js
@@ -1,13 +1,70 @@
 // TODO: Include packages needed for this application
+// Inquirer has been installed
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+},
+{
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+},
+{
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+},
+{
+    type: 'input',
+    name: 'license',
+    message: 'What is the license for your project? Enter "None" if there is no license:',
+},
+{
+    type: 'input',
+    name: 'username',
+    message: 'Enter your GitHub username:',
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('README.md created successfully!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        answers.license = answers.license.toLowerCase();
+        const markdown = generateMarkdown(answers);
+        writeToFile('README.md', markdown);
+  });
+}
 
 // Function call to initialize app
 init();
